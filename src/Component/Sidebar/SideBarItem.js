26bf058609a6633ca634f6e90c2a8b3af36a67d9
kesava07 <kesava07.ip@gmail.com>
2021-10-08 20:59:16 +0530

    Improvement

---
+++ src/Component/Sidebar/SideBarItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const SideBarItem = ({ name, link, icon }) => {
+    return (
+        <li>
+            <NavLink activeClassName="active" exact to={link}>{icon}{name}</NavLink>
+        </li>
+    )
+}
+
+export default SideBarItem;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,155 +1,57 @@
-import { BiLike } from 'react-icons/bi';
-import { BiDislike } from 'react-icons/bi';
-import React, { useState, useEffect } from 'react';
+import { BiLike } from "react-icons/bi";
+import { BiDislike } from "react-icons/bi";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
-import Header from '../Component/Header Component/Header';
-
-
-
+import Header from "../Component/Header/Header";
+import VideoCard from "../Component/VideoCard/VideoCard";
 
 const Home = (props) => {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState("");
+  console.log("Search state data", searchResults);
 
-    const [data, setData] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
-    const [searchResults, setSearchResults] = useState("");
-    console.log("Search state data",searchResults);
-
-
-    const searchHandler = (searchTerm) => {
-        // console.log("serachwords:",searchTerm);
-        setSearchTerm(searchTerm);
-        if (searchTerm !== "") {
-            const newDataList = data.filter((users) => {
-                return Object.values(users).join(" ")
-                    .toLowerCase()
-                    .includes(searchTerm.toLowerCase());
-            });
-            setSearchResults(newDataList);
-            // console.log("vvv:", newDataList);
-        }
-        else {
-            setSearchResults(data);
-        }
+  const searchHandler = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    if (searchTerm !== "") {
+      const newDataList = data.filter((users) => {
+        return Object.values(users)
+          .join(" ")
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
+      });
+      setSearchResults(newDataList);
+      // console.log("vvv:", newDataList);
+    } else {
+      setSearchResults(data);
     }
+  };
 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((data) => {
+        // console.log("Data:",data)
+        setData(data);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then(resp => resp.json())
-            .then(data => {
-                // console.log("Data:",data)
-                setData(data)
-            });
-    }, []);
-
-
-
-
-
-
-    // return (
-    //     <div>
-    //         <Header
-    //             term={searchTerm}
-    //             searchKeyword={searchHandler}
-    //         />
-    //         <br />
-    //         <div className="card-section">
-
-    //             {
-
-    //                 data .map((users) => {
-    //                     return (
-
-    //                         <div className="card-items">
-
-    //                             <div className="video-section">
-    //                                 {users.id} Name:-{users.name}
-    //                             </div>
-
-    //                             <div className="card-btns">
-    //                                 <button className="Like-Button"> {users.id} <BiLike /></button>
-    //                                 <p> Name{users.address.street}</p>
-
-    //                                 <button className="Dislike-Button">{users.id} <BiDislike /> </button>
-    //                             </div>
-    //                         </div>
-    //                     )
-    //                 }
-    //                 )
-
-    //             }
-
-    //         </div>
-
-
-    //     </div>
-
-
-    // )
-
+  const displayVideos = (data) => {
     return (
-        <div>
-            <Header term={searchTerm} searchKeyword={searchHandler} />
+      data && data.map((user, index) => <VideoCard user={user} key={index} />)
+    );
+  };
 
-            <div className="card-section">
+  return (
+    <div>
+      <Header isShowInput term={searchTerm} searchKeyword={searchHandler} />
 
-                {
-                    searchResults.length > 0 ? (
-                        searchResults.map((users) => {
-                            console.log("Filtered users:",users)
-                            return (
-                                <div className="card-items">
-
-                                    <div className="video-section">
-                                        {users.id} Name:-{users.name}
-                                    </div>
-
-                                    <div className="card-btns">
-                                        <button className="Like-Button"> {users.id} <BiLike /></button>
-                                        <p> Name{users.address.street}</p>
-
-                                        <button className="Dislike-Button">{users.id} <BiDislike /> </button>
-                                    </div>
-                                </div>
-
-                            )
-                        })
-                    ) : (
-                        
-                        data.map((users) => {
-                            console.log("Total users:",users);
-                            return (
-                                
-                                <div className="card-items">
-                                    
-
-                                    <div className="video-section">
-                                        {users.id} Name:-{users.name}
-                                    </div>
-
-                                    <div className="card-btns">
-                                        <button className="Like-Button"> {users.id} <BiLike /></button>
-                                        <p> Name{users.address.street}</p>
-
-                                        <button className="Dislike-Button">{users.id} <BiDislike /> </button>
-                                    </div>
-                                </div>
-
-                            )
-                        })
-                    )
-                }
-            </div>
-
-        </div>
-
-
-
-
-    )
-
-
-}
+      <div className="card-section">
+        {searchResults.length > 0
+          ? displayVideos(searchResults)
+          : displayVideos(data)}
+      </div>
+    </div>
+  );
+};
 export default Home;
-
