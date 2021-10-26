6f919eea94b59144a220561d2b86a89145cadb4f
vin4math <vin4math@gmail.com>
2021-10-27 03:31:08 +0530

    final commit with husky

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-
-
-
 import Home from './Pages/Home';
-
-
 
 
 
 import Upload from './Pages/Upload';
 import About from './Pages/About';
-import {
-  BrowserRouter as Router,
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 
@@ -18,39 +12,18 @@ import {
 
 
 
-  Route
-} from 'react-router-dom';
+
+
 
 function App() {
-
-
   return (
     <div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
       <Router>
-
         {/* <Header /> */}
         <div className="main">
           <Route exact path="/" component={Home} />
           <Route exact path="/upload" component={Upload} />
           <Route path="/about" component={About} />
-
         </div>
       </Router>
     </div>
